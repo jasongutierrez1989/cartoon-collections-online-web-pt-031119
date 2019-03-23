1f0c0801d435969f1d55fb5f1d8c711b254e965f
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-23 04:55:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,20 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet(array)
   array.map { |name| name.capitalize + '!'  }
-  # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  while i < array.length
+    if array[i].length > 4
+      i += 1
+      return true
+    else
+      i += 1
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
